0cc2982fa9e4110c86ecfdfcdd8d5dcdbb3e5e1e
mipaulius <mi.paulius@gmail.com>
2024-05-21 16:01:28 +0300

    Update 2021-05-21

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 15f;
+    [SerializeField] float boostSpeed = 30f;
+
 
     // Update is called once per frame
     void Update()
     {
-                transform.Rotate(0, 0, 0.1f);
-                transform.Translate(0, 0.01f, 0);
+                float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+                float moveAmount = Input.GetAxis("Vertical") * moveSpeed  * Time.deltaTime;
+                transform.Rotate(0, 0, -steerAmount);
+                transform.Translate(0, moveAmount, 0);
+    }
 
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost!")
+        {
+            // Debug.Log("You are boosting, man!"); checking if it is working
+            moveSpeed = boostSpeed;
+        }
     }
 }
